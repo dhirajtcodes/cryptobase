a84773ba732e4712249bf43c2e307da70d53af16
dtandulkar <dtandulkar1998@gmail.com>
2022-05-08 23:52:42 +0530

    homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Banner from '../components/Banner/Banner'
 import CoinTable from '../components/CoinTable/CoinTable'
+import { useNavigate } from "react-router-dom"
 
 const HomePage = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        navigate("/", { replace: true })
+    }, [])
     return (
         <>
             <Banner />
